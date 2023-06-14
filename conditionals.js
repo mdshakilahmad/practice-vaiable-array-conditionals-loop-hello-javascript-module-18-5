aa30eb919b98abde55297d1ec25a06033fafbb10
shakil <ahmadshakil22255@gmail.com>
2023-06-14 06:19:15 +0600

    first commit

---
+++ conditionals.js
@@ -0,0 +1,13 @@
+/*
+--------------- 
+CONDITIONALS
+---------------
+1. Meaning of: >, <, >=, <=, ==, !=, ===, !===,
+2. Meaning of &&
+3. Meaning of ||
+4. Let's say you have x amount of money. if you have more than 80,000, then you will buy a mac
+  if more than 60,000 then you will buy gaming laptop
+  if you more then 40,000 then you will buy lenovo yoga
+  if you have more than 20,000 then you will by an used  laptop
+  otherwise, you will use you mobile phone
+*/
---
+++ variable.js
@@ -0,0 +1,18 @@
+/* 
+-----------------
+VARIABLE
+-----------------
+1. what is javascript?
+2. How does JS work?
+3. What is variable?
+4. Declare a variable?
+5. Type of variable? How can you find out type of a variable
+6. Primitive and non-primitive data types
+7. Naming convention of js variables
+8. Math Operation +, -, *, /, %
+9. Short hand: +=, -=, *=, /=
+10. ++, --,
+11, parseInt, parseFloat
+12. toFixed
+
+*/
